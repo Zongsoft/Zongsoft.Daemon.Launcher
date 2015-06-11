56a7be7c2e94c97bab6398a63563911ec36b2aba
Popeye Zhong <zongsoft@gmail.com>
2015-06-11 17:24:07 +0800

    增加了“部署文件(deploy.ini)”和“部署脚本文件(deploy-debug.bat, deploy-release.bat)”。

---
+++ deploy-debug.bat
@@ -0,0 +1,1 @@
+\Zongsoft\Zongsoft.Utilities.Deployer\src\bin\Debug\Zongsoft.Utilities.Deployer.exe -edition:Debug "deploy.ini"
---
+++ deploy-release.bat
@@ -0,0 +1,1 @@
+\Zongsoft\Zongsoft.Utilities.Deployer\src\bin\Debug\Zongsoft.Utilities.Deployer.exe -edition:Release "deploy.ini"
